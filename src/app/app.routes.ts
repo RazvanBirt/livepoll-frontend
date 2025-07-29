84a844ada6974b561e60c7c10dd0978c9529f404
RazvanBirt <razvan7birt@gmail.com>
2025-07-29 20:31:29 +0100

    add DaisyUI and Tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [] = [
+  //TODO : registar routes aqui
+  // aplicar lazy loading
+  // aplicar router outlet app.ts
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('livepoll-frontend');
+  protected readonly title = signal('LivePoll');
 }
