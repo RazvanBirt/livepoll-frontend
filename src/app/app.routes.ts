1194abc6e9c92b0e3e390ef3848e0487918e3e1e
RazvanBirt <razvan7birt@gmail.com>
2025-07-29 21:20:56 +0100

    simple lazy loading routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [] = [
-  //TODO : registar routes aqui
-  // aplicar lazy loading
-  // aplicar router outlet app.ts
+  {
+    path: 'home',
+    loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full' as const
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ];
---
+++ src/home/home.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.html'
+})
+
+export class HomeComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+}
+
